c96449b48604dd8a36d1d541e61a380bc063e2fa
vikramtk <83641234+vikramtk@users.noreply.github.com>
2021-08-25 14:43:24 +0530

    Update onsite1.js

--- onsite1 new/scripts/onsite1.js
+++ onsite1 new/scripts/onsite1.js
@@ -5,22 +5,43 @@ var salad = 0;
 var bacon = 0;
 var cheese = 0;
 var meat = 0;
+var s=0;
+var b=0;
+var c=0;
+var m=0;
+var arrs=[];
+var arrb=[];
+var arrc=[];
+var arrm=[];
+
+const burgertop = document.getElementById('top');
+
+var topimg = new Image();
+topimg.src = "images/top.png";
+burgertop.appendChild(topimg);
+
+var botimg = new Image();
+botimg.src = "images/bottom.png";
+burgertop.appendChild(botimg);
 
 const sal = document.getElementById('sal');
-const burgertop1 = document.getElementById('top1');
-const burgertop2 = document.getElementById('top2');
-const burgertop3 = document.getElementById('top3');
-const burgertop4 = document.getElementById('top4');
 
 let Buttons5 = document.getElementById('b5');
 Buttons5.onclick = function()
 {
   salad++;
   sal.innerHTML = salad;
-  var h=parseInt(burgertop1.style.height);
-  burgertop1.style.height = (h+20) + "px";
   price += 0.40;
   prices.innerHTML = price.toFixed(2);
+
+  burgertop.removeChild(botimg);
+  var salimg = new Image();
+  salimg.src = "images/salad.png";
+  burgertop.appendChild(salimg);
+  burgertop.appendChild(botimg);
+
+  arrs[s] = salimg;
+  s++;
 }
 
 let Buttons1 = document.getElementById('b1');
@@ -28,10 +49,10 @@ Buttons1.onclick = function()
 {
   if(salad>0){
   salad--;
-  var h=parseInt(burgertop1.style.height);
-  burgertop1.style.height = (h-20) + "px";
   price -= 0.40;
   prices.innerHTML = price.toFixed(2);
+  s--;
+  burgertop.removeChild(arrs[s]);
   }
   sal.innerHTML = salad;
 }
@@ -43,10 +64,17 @@ Buttons6.onclick = function()
 {
   bacon++;
   bac.innerHTML = bacon;
-  var h=parseInt(burgertop2.style.height);
-  burgertop2.style.height = (h+25) + "px";
   price += 1.20;
   prices.innerHTML = price.toFixed(2);
+
+  burgertop.removeChild(botimg);
+  var bacimg = new Image();
+  bacimg.src = "images/bacon.png";
+  burgertop.appendChild(bacimg);
+  burgertop.appendChild(botimg);
+
+  arrb[b] = bacimg;
+  b++;
 }
 
 let Buttons2 = document.getElementById('b2');
@@ -54,10 +82,10 @@ Buttons2.onclick = function()
 {
   if(bacon>0){
   bacon--;
-  var h=parseInt(burgertop2.style.height);
-  burgertop2.style.height = (h-25) + "px";
   price -= 1.20;
   prices.innerHTML = price.toFixed(2);
+  b--;
+  burgertop.removeChild(arrb[b]);
   }
   bac.innerHTML = bacon;
 }
@@ -69,10 +97,17 @@ Buttons7.onclick = function()
 {
   cheese++;
   che.innerHTML = cheese;
-  var h=parseInt(burgertop3.style.height);
-  burgertop3.style.height = (h+10) + "px";
   price += 0.80;
   prices.innerHTML = price.toFixed(2);
+
+  burgertop.removeChild(botimg);
+  var cheimg = new Image();
+  cheimg.src = "images/cheese.png";
+  burgertop.appendChild(cheimg);
+  burgertop.appendChild(botimg);
+
+  arrc[c] = cheimg;
+  c++;
 }
 
 let Buttons3 = document.getElementById('b3');
@@ -80,10 +115,10 @@ Buttons3.onclick = function()
 {
   if(cheese>0){
   cheese--;
-  var h=parseInt(burgertop3.style.height);
-  burgertop3.style.height = (h-10) + "px";
   price -= 0.80;
   prices.innerHTML = price.toFixed(2);
+  c--;
+  burgertop.removeChild(arrc[c]);
   }
   che.innerHTML = cheese;
 }
@@ -95,10 +130,17 @@ Buttons8.onclick = function()
 {
   meat++;
   mea.innerHTML = meat;
-  var h=parseInt(burgertop4.style.height);
-  burgertop4.style.height = (h+20) + "px";
   price += 1.80;
   prices.innerHTML = price.toFixed(2);
+
+  burgertop.removeChild(botimg);
+  var meaimg = new Image();
+  meaimg.src = "images/meat.png";
+  burgertop.appendChild(meaimg);
+  burgertop.appendChild(botimg);
+
+  arrm[m] = meaimg;
+  m++;
 }
 
 let Buttons4 = document.getElementById('b4');
@@ -106,15 +148,47 @@ Buttons4.onclick = function()
 {
   if(meat>0){
   meat--;
-  var h=parseInt(burgertop4.style.height);
-  burgertop4.style.height = (h-20) + "px";
   price -= 1.80;
   prices.innerHTML = price.toFixed(2);
+  m--;
+  burgertop.removeChild(arrm[m]);
   }
   mea.innerHTML = meat;
 }
 
 let Buttons9 = document.getElementById('b9');
 Buttons9.onclick = function(){
-    alert("Order Placed" + "\n" + "Total Price is " + price.toFixed(2));
+  alert("Order Placed" + "\n" + "Total Price is " + price.toFixed(2));
+
+  price = 4.00;
+  salad = 0;
+  bacon = 0;
+  cheese = 0;
+  meat = 0;
+  
+  sal.innerHTML = salad;
+  bac.innerHTML = bacon;
+  che.innerHTML = cheese;
+  mea.innerHTML = meat;
+  prices.innerHTML = price.toFixed(2);
+
+  while(s>0){
+    s--;
+    burgertop.removeChild(arrs[s]);
+  }
+
+  while(b>0){
+    b--;
+    burgertop.removeChild(arrb[b]);
+  }
+
+  while(c>0){
+    c--;
+    burgertop.removeChild(arrc[c]);
+  }
+
+  while(m>0){
+    m--;
+    burgertop.removeChild(arrm[m]);
+  }
 }
